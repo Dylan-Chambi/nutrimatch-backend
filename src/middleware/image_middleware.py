85618635cd4da2536b4c042da38db62891fc268e
Dylan-Chambi <dylanchambifi@gmail.com>
2024-02-20 11:27:49 -0400

    Support for webp images format

--- src/middleware/image_middleware.py
+++ src/middleware/image_middleware.py
@@ -7,10 +7,10 @@ from PIL import Image
 class ImageValidationMiddleware:
     def __call__(self, file: UploadFile = File(...)):
         # Check if the content type is an image
-        if not file.content_type in ["image/jpeg", "image/png", "image/jpg"]:
+        if not file.content_type in ["image/jpeg", "image/png", "image/jpg", "image/webp"]:
             raise HTTPException(
                 status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-                detail="Only images with jpeg, jpg or png format are supported"
+                detail="Only images with jpeg, jpg, png, and webp formats are supported"
             )
         
         # Check if the image is corrupted
