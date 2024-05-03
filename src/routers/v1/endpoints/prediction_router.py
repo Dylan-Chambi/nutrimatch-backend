925d9be3c510a3fa9aeebd4bd8e65dd2c8b5b757
Dylan-Chambi <dylanchambifi@gmail.com>
2024-05-03 19:32:42 -0400

    Rename 'food-detection' router to 'prediction'

--- src/routers/v1/endpoints/prediction_router.py
+++ src/routers/v1/endpoints/prediction_router.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, UploadFile, status, Depends
 from typing import Annotated
-from src.controller.image_controller import image_detect_food, get_recomendation_by_image
+from src.controller.prediction_controller import image_detect_food, get_recomendation_by_image
 from src.middleware.image_middleware import ImageValidationMiddleware
 from src.service.image_pred_service import ImagePredictionService
 from src.model.general_database import GeneralDatabase
 from src.service.mongodb.mongodb_service import MongoDBService
-from src.service.firebase.firestore_service import FirestoreService
 from src.service.firebase.firebase_storage_service import FirebaseStorageService
 from src.service.recomendation_service import RecommendationService
 from src.middleware.auth_middleware import authentication_jwt_middleware
@@ -16,7 +15,7 @@ from src.config.logger import logger
 
 
 
-food_detec_router = APIRouter()
+prediction_router = APIRouter()
 
 
 
@@ -34,7 +33,7 @@ def get_database_service():
     return MongoDBService(firebase_storage_service)
 
 
-@food_detec_router.get("/health-check", status_code=status.HTTP_200_OK)
+@prediction_router.get("/health-check", status_code=status.HTTP_200_OK)
 async def health_check():
     """
     Health check endpoint
@@ -43,8 +42,12 @@ async def health_check():
     return {"message": "Food detection endpoint is healthy!"}
 
 
-@food_detec_router.post('/image')
-def detect_food(file: UploadFile = Depends(ImageValidationMiddleware()), img_pred_service: ImagePredictionService = Depends(get_image_pred_service)):
+@prediction_router.post('/food-detection')
+def detect_food(
+        user_info: Annotated[UserRecord, Depends(authentication_jwt_middleware)],
+        file: UploadFile = Depends(ImageValidationMiddleware()), 
+        img_pred_service: ImagePredictionService = Depends(get_image_pred_service)
+    ):
     """
     Detect food in an image
     """
@@ -52,7 +55,7 @@ def detect_food(file: UploadFile = Depends(ImageValidationMiddleware()), img_pre
     return image_detect_food(file, img_pred_service)
 
 
-@food_detec_router.post('/recommendation')
+@prediction_router.post('/recommendation')
 def recommend_food(
         user_info: Annotated[UserRecord, Depends(authentication_jwt_middleware)],
         file: UploadFile = Depends(ImageValidationMiddleware()), 
--- src/routers/v1/router_v1.py
+++ src/routers/v1/router_v1.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from src.routers.v1.endpoints import image_router, recommendation_router
+from src.routers.v1.endpoints import prediction_router
+from src.routers.v1.endpoints import recommendation_router
 
 api_router = APIRouter()
 
-api_router.include_router(image_router.food_detec_router, prefix="/food-detection", tags=["food-detection"])
+api_router.include_router(prediction_router.prediction_router, prefix="/prediction", tags=["prediction"])
 api_router.include_router(recommendation_router.recommendation_router, prefix="/recommendation", tags=["recommendation"])
